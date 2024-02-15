d3e7d876c0848fc3ad169b82dbc2c412b003defc
Dika Pangestu <dikdika2406@gmail.com>
2024-02-15 07:12:37 +0700

    front_end-react

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt='g letter'/>
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <p className='p__opensans'>From exquisite culinary creations to impeccable service, this subheading encapsulates the essence of an indulgent journey through the culinary arts. Discover the harmony of flavors, the artistry of presentation, and the sophistication of ambiance, as this key opens the door to a realm where each meal becomes a symphony of taste and refinement.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='abou knife' />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <p className='p__opensans'>From exquisite culinary creations to impeccable service, this subheading encapsulates the essence of an indulgent journey through the culinary arts. Discover the harmony of flavors, the artistry of presentation, and the sophistication of ambiance, as this key opens the door to a realm where each meal becomes a symphony of taste and refinement.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default AboutUs;
