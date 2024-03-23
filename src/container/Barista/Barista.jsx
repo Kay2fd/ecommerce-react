cdd64551e6e9429503ed44e99a3d168216bf768d
Dika Pangestu <dikdika2406@gmail.com>
2024-03-23 19:40:54 +0700

    barista

--- src/container/Barista/Barista.jsx
+++ src/container/Barista/Barista.jsx
@@ -1,29 +1,71 @@
-import React from 'react';
-
-import { SubHeading}  from '../../components';
-import { images } from '../../constants';
-import './Barista.css';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Barista.css";
 
 const Barista = () => (
-  <div className='app__bg app__wrapper section__padding'>
-    <div className='app__wrapper_img app__wrapper_img-reverse'>
-      <img src={images.barista} alt="Barista" />
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_content">
+      <div className="app__wrapper_img">
+        <img src={images.barista} alt="Barista" />
+      </div>
+
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef" />
+        <h1 className="headtext_cormorant">Tentang Kopi</h1>
+
+        <div className="app__barista-content">
+          <div className="app__barista-content_text_chef">
+            <p className="p_opensans" style={{ color: "#AAA" }}>
+              "Kopi adalah seni di mana aroma, kesempurnaan, dan keahlian
+              menyatu dalam setiap cangkir yang kami sajikan dengan penuh cinta
+              dan dedikasi untuk memanjakan lidah Anda. Kami, sebagai barista,
+              percaya bahwa setiap cangkir kopi adalah karya seni yang unik.
+              Dengan setiap sajian, kami tidak hanya menghidupkan rasa, tetapi
+              juga mengundang Anda untuk merasakan keindahan di balik setiap
+              tegukan, dan itulah yang membuat kami bersemangat dan berdedikasi
+              setiap hari."
+            </p>
+          </div>
+
+          <div className="app__barista-name app__barista-content_text_chef">
+            <p>Chef Juna</p>
+            <p className="p__opensans">Barista & Founder</p>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div className='app__wrapper_info'>
-      <SubHeading title='Barista'/>
-      <h1 className='headtext_cormorant'>Tentang Kopi</h1>
 
-      <div className='app__barista-content'>
-        <div className='app__barista-content_text'>
-          <p className='p_opensans' style={{color: '#AAA'}}>"Kopi adalah seni di mana aroma, kesempurnaan, dan keahlian menyatu dalam setiap cangkir yang kami sajikan dengan penuh cinta dan dedikasi untuk memanjakan lidah Anda. Kami, sebagai barista, percaya bahwa setiap cangkir kopi adalah karya seni yang unik. Dengan setiap sajian, kami tidak hanya menghidupkan rasa, tetapi juga mengundang Anda untuk merasakan keindahan di balik setiap tegukan, dan itulah yang membuat kami bersemangat dan berdedikasi setiap hari."</p>
-        </div>
-        
-        <div className='app__barista-name'>
-          <p>Dika P.</p>
-          <p className='p__opensans'>Barista & Founder</p>
-        </div>
+{/* konten atas */}
+    <div className="app__wrapper_content img_barista">
+      <div className="app__wrapper_info">
+        <SubHeading title="Barista" />
+        <h1 className="headtext_cormorant">Tentang Kopi</h1>
 
+        <div className="app__barista-content">
+          <div className="app__barista-content_text">
+            <p className="p_opensans" style={{ color: "#AAA" }}>
+              "Kopi adalah seni di mana aroma, kesempurnaan, dan keahlian
+              menyatu dalam setiap cangkir yang kami sajikan dengan penuh cinta
+              dan dedikasi untuk memanjakan lidah Anda. Kami, sebagai barista,
+              percaya bahwa setiap cangkir kopi adalah karya seni yang unik.
+              Dengan setiap sajian, kami tidak hanya menghidupkan rasa, tetapi
+              juga mengundang Anda untuk merasakan keindahan di balik setiap
+              tegukan, dan itulah yang membuat kami bersemangat dan berdedikasi
+              setiap hari."
+            </p>
+          </div>
+
+          <div className="app__barista-name">
+            <p>Dika P.</p>
+            <p className="p__opensans">Barista & Founder</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.barista} alt="Barista" />
       </div>
     </div>
   </div>
