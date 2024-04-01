7d46863eaf8bdb1d7490660a4367c08a064c84b3
Dika Pangestu <dikdika2406@gmail.com>
2024-04-01 22:53:16 +0700

    add poto konten

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Barista, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Barista, FindUs, Footer, Gallery, Header, Intro, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -12,7 +12,6 @@ const App = () => (
     <SpecialMenu />
     <Barista />
     <Intro />
-    <Laurels />
     <Gallery />
     <FindUs />
     <Footer />
--- src/container/Barista/Barista.jsx
+++ src/container/Barista/Barista.jsx
@@ -5,39 +5,13 @@ import "./Barista.css";
 
 const Barista = () => (
   <div className="app__bg app__wrapper section__padding">
-    <div className="app__wrapper_content">
-      <div className="app__wrapper_img">
-        <img src={images.barista} alt="Barista" />
-      </div>
 
-      <div className="app__wrapper_info">
-        <SubHeading title="Chef" />
-        <h1 className="headtext_cormorant">Tentang Kopi</h1>
-
-        <div className="app__barista-content">
-          <div className="app__barista-content_text_chef">
-            <p className="p_opensans" style={{ color: "#AAA" }}>
-              "Kopi adalah seni di mana aroma, kesempurnaan, dan keahlian
-              menyatu dalam setiap cangkir yang kami sajikan dengan penuh cinta
-              dan dedikasi untuk memanjakan lidah Anda. Kami, sebagai barista,
-              percaya bahwa setiap cangkir kopi adalah karya seni yang unik.
-              Dengan setiap sajian, kami tidak hanya menghidupkan rasa, tetapi
-              juga mengundang Anda untuk merasakan keindahan di balik setiap
-              tegukan, dan itulah yang membuat kami bersemangat dan berdedikasi
-              setiap hari."
-            </p>
-          </div>
-
-          <div className="app__barista-name app__barista-content_text_chef">
-            <p>Chef Juna</p>
-            <p className="p__opensans">Barista & Founder</p>
-          </div>
-        </div>
-      </div>
+  <div className="app__wrapper_content">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.barista} alt="" />
     </div>
-
-
-{/* konten atas */}
+  </div>
+  
     <div className="app__wrapper_content img_barista">
       <div className="app__wrapper_info">
         <SubHeading title="Barista" />
@@ -62,10 +36,6 @@ const Barista = () => (
             <p className="p__opensans">Barista & Founder</p>
           </div>
         </div>
-      </div>
-
-      <div className="app__wrapper_img">
-        <img src={images.barista} alt="Barista" />
       </div>
     </div>
   </div>
